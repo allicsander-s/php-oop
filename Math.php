204b8196dde3e5af1cbeea10c9560eba1f880494
allicsander <allicsander@example.com>
2019-10-31 18:32:45 +0300

    some phpdoc for the class

--- Math.php
+++ Math.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * this is my splendid Math class for different  oop demos
+ * @author Sasha <allicsander@example.com>
+ * @copyright 2019
+ * @license gnu free
+ */
 class Math{
   
   /**
