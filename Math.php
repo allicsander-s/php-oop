6f7e57df3cc9094deda4eb1978354dad1c794146
allicsander <allicsander@example.com>
2019-10-31 18:24:11 +0300

    added another param for the method

--- Math.php
+++ Math.php
@@ -5,8 +5,9 @@ class Math{
   /**
    * 
    * @param string 	$stuff  what a strange method
+   * @param string 	$stuff2  what a wonderful method
    */
-  function doSomethin($stuff){
+  function doSomethin($stuff, $stuff2){
   	echo 'doing ' . $stuff ;
   }
 
