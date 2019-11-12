26ab9019a14e585e8986120a74c7065eb5fb134b
allicsander <allicsander@example.com>
2019-11-12 08:53:39 +0300

    updated dynamicBindResults method

--- MySqlDb.php
+++ MySqlDb.php
@@ -18,6 +18,7 @@ class MySqlDb
     $stmt = $this->_prepareQuery();
     $stmt->execute();
     $results = $this->_dynamicBindResults($stmt);
+    return $results;
 	} 
 
 	public function get($tableName, $nubRows = null)
@@ -47,10 +48,27 @@ class MySqlDb
 
 	protected function _dynamicBindResults($stmt)
 	{
+    $parameters = array(); 
+    $results = array();
+
     $meta = $stmt->result_metadata();
+
     while($field = $meta->fetch_field()){
-    	print_r($field);
+    	$parameters[] = &$row[$field->name];
     }
+
+    call_user_func_array( array($stmt, 'bind_result'), $parameters);
+
+    while ( $stmt->fetch() ) {
+    	$x = array();
+    	foreach ($row as $key => $val) {
+    		$x[$key] = $val;
+    	}
+    	$results[] = $x;
+    }
+
+    return $results;
+
 	}
 
 	protected function _prepareQuery()
